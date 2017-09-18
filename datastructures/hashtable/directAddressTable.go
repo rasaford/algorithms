9d778cf1dd4ce38c673eb7e449469f1f17736bff
Max Fr <fruehaufmaximilian@gmail.com>
2017-09-18 16:50:13 +0200

    add direct address table documentation

--- datastructures/hashtable/directAddressTable.go
+++ datastructures/hashtable/directAddressTable.go
@@ -9,6 +9,10 @@ type directAddress struct {
 	uMin, uMax int
 }
 
+// NewDirectAddressTable creates a new array with indices in the range:
+// keyMIn <= x < keyMax.
+//
+// It requires O(u) space with u := keyMax - keyMIn
 func NewDirectAddressTable(keyMin, keyMax int) *directAddress {
 	if keyMin >= keyMax {
 		return nil
@@ -21,6 +25,10 @@ func NewDirectAddressTable(keyMin, keyMax int) *directAddress {
 	}
 }
 
+// Insert inserts a new key, value pair in the table if the key is in the range of
+// valid keys.
+//
+// It runs in O(1) time
 func (d *directAddress) Insert(key int, value interface{}) {
 	if err := d.validateKey(key); err != nil {
 		return
@@ -28,6 +36,10 @@ func (d *directAddress) Insert(key int, value interface{}) {
 	d.array[key-d.uMin] = value
 }
 
+// Search returns the stored value for the given key if it is in the range of
+// valid keys.
+//
+// It runs in O(1) time
 func (d *directAddress) Search(key int) (interface{}, error) {
 	if err := d.validateKey(key); err != nil {
 		return nil, err
@@ -35,6 +47,9 @@ func (d *directAddress) Search(key int) (interface{}, error) {
 	return d.array[key-d.uMin], nil
 }
 
+// Delete removes a key form the table if it is in the range of valid keys.
+//
+// It runs in O(1) time
 func (d *directAddress) Delete(key int) {
 	if err := d.validateKey(key); err != nil {
 		return
