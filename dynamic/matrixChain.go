61f6aac56688679dfef6b8eea89ce9a65f47c2bc
Max Fr <fruehaufmaximilian@gmail.com>
2017-09-28 18:21:14 +0200

    add matrix chain multiplication

--- dynamic/matrixChain.go
+++ dynamic/matrixChain.go
@@ -3,7 +3,44 @@ package dynamic
 import (
 	"fmt"
 	"math"
+	"strconv"
+
+	"github.com/rasaford/algorithms/divideAndConquer/matrix"
+	"github.com/rasaford/algorithms/internal/helper"
 )
+
+func MatrixChainMultiply(matrices [][][]int) ([][]int, error) {
+	rowCount, err := getRowCount(matrices)
+	if err != nil {
+		return nil, err
+	}
+	_, store := MatrixChainOrder(rowCount)
+	return multiplyPairs(matrices, store, 1, len(matrices)-1), nil
+}
+
+func getRowCount(matrices [][][]int) ([]int, error) {
+	rowCount := make([]int, len(matrices))
+	for i := range rowCount {
+		rowCount[i] = len(matrices[i])
+		if i != 0 && len(matrices[i-1][0]) != len(matrices[i]) {
+			return nil, fmt.Errorf("column of matrix %d is not equal to row of matrix %d", i-1, i)
+		}
+	}
+	return rowCount, nil
+}
+
+func multiplyPairs(matrices [][][]int, store map[string]int, i, j int) [][]int {
+	if i == j {
+		return matrices[i]
+	}
+	split := store[str(i, j)]
+	// no error can occur in MatrixMultiply because it has already been checked that
+	// the column count of the first matrix is the same as thr row count on the second.
+	res, _ := matrix.Multiply(
+		multiplyPairs(matrices, store, i, split),
+		multiplyPairs(matrices, store, split+1, j))
+	return res
+}
 
 // MatrixChainOrder takes an array of row counts for n matrices and computes the best way to
 // parenthesize multiplying all of them together.
@@ -12,7 +49,7 @@ import (
 // multiplications required can be greatly reduced.
 // The product of a p x q and a q x r matrix has the dimensions: p x r
 //
-// It runs in O(n^2) time, wehre n := len(matrixRow)
+// It runs in O(n^3) time, wehre n := len(matrixRow)
 func MatrixChainOrder(matrixRow []int) (map[string]int, map[string]int) {
 	n := len(matrixRow) - 1
 	matrix, store := make(map[string]int), make(map[string]int)
@@ -32,6 +69,24 @@ func MatrixChainOrder(matrixRow []int) (map[string]int, map[string]int) {
 		}
 	}
 	return matrix, store
+}
+
+func PrintOptimal(matrixRow []int) (string, string) {
+	matrix, store := MatrixChainOrder(matrixRow)
+	n := len(matrixRow) - 1
+	cost := strconv.Itoa(matrix[str(1, n)])
+	parens := printParens(store, 1, n)
+	return cost, parens
+}
+
+func printParens(store map[string]int, i, j int) string {
+	if i == j {
+		return fmt.Sprintf("A_%d", i)
+	}
+	return helper.Concat("(",
+		printParens(store, i, store[str(i, j)]),
+		printParens(store, store[str(i, j)]+1, j),
+		")")
 }
 
 func str(a, b int) string { return fmt.Sprintf("%d-%d", a, b) }
--- dynamic/matrixChain_test.go
+++ dynamic/matrixChain_test.go
@@ -18,40 +18,8 @@ func TestMatrixChainOrder(t *testing.T) {
 		{
 			"book example",
 			args{[]int{30, 35, 15, 5, 10, 20, 25}},
-			map[string]int{
-				str(1, 3): 7875,
-				str(1, 5): 11875,
-				str(2, 6): 10500,
-				str(1, 4): 9375,
-				str(2, 5): 7125,
-				str(2, 3): 2625,
-				str(3, 4): 750,
-				str(4, 5): 1000,
-				str(2, 4): 4375,
-				str(4, 6): 3500,
-				str(1, 2): 15750,
-				str(5, 6): 5000,
-				str(3, 5): 2500,
-				str(3, 6): 5375,
-				str(1, 6): 15125,
-			},
-			map[string]int{
-				str(2, 5): 3,
-				str(2, 3): 2,
-				str(3, 4): 3,
-				str(5, 6): 5,
-				str(2, 4): 3,
-				str(3, 6): 3,
-				str(4, 6): 5,
-				str(1, 5): 3,
-				str(2, 6): 3,
-				str(1, 6): 3,
-				str(1, 2): 1,
-				str(1, 3): 1,
-				str(3, 5): 3,
-				str(4, 5): 4,
-				str(1, 4): 3,
-			},
+			map[string]int{str(1, 3): 7875, str(1, 5): 11875, str(2, 6): 10500, str(1, 4): 9375, str(2, 5): 7125, str(2, 3): 2625, str(3, 4): 750, str(4, 5): 1000, str(2, 4): 4375, str(4, 6): 3500, str(1, 2): 15750, str(5, 6): 5000, str(3, 5): 2500, str(3, 6): 5375, str(1, 6): 15125},
+			map[string]int{str(2, 5): 3, str(2, 3): 2, str(3, 4): 3, str(5, 6): 5, str(2, 4): 3, str(3, 6): 3, str(4, 6): 5, str(1, 5): 3, str(2, 6): 3, str(1, 6): 3, str(1, 2): 1, str(1, 3): 1, str(3, 5): 3, str(4, 5): 4, str(1, 4): 3},
 		},
 	}
 	for _, tt := range tests {
@@ -62,6 +30,112 @@ func TestMatrixChainOrder(t *testing.T) {
 			}
 			if !reflect.DeepEqual(store, tt.want1) {
 				t.Errorf("MatrixChainOrder() store = %v, want %v", store, tt.want1)
+			}
+		})
+	}
+}
+
+func TestPrintOptimal(t *testing.T) {
+	type args struct {
+		matrixRow []int
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  string
+		want1 string
+	}{
+		{
+			"book example",
+			args{[]int{30, 35, 15, 5, 10, 20, 25}},
+			"15125",
+			"((A_1(A_2A_3))((A_4A_5)A_6))",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cost, parens := PrintOptimal(tt.args.matrixRow)
+			if cost != tt.want {
+				t.Errorf("PrintOptimal() cost = %v, want %v", cost, tt.want)
+			}
+			if parens != tt.want1 {
+				t.Errorf("PrintOptimal() parens = %v, want %v", parens, tt.want1)
+			}
+		})
+	}
+}
+
+func TestMatrixChainMultiply(t *testing.T) {
+	type args struct {
+		matrices [][][]int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    [][]int
+		wantErr bool
+	}{
+		{
+			"5x7 example",
+			args{[][][]int{
+				[][]int{
+					[]int{1, 2, 3, 4, 5},
+					[]int{6, 7, 8, 9, 10},
+					[]int{11, 12, 13, 14, 15},
+					[]int{16, 17, 18, 19, 20},
+				},
+				[][]int{
+					[]int{1, 2},
+					[]int{3, 4},
+					[]int{5, 6},
+					[]int{7, 8},
+					[]int{9, 10},
+				},
+				[][]int{
+					[]int{1, 2},
+					[]int{3, 4},
+				},
+				[][]int{
+					[]int{1, 2, 3, 4, 5, 6, 7},
+					[]int{8, 9, 10, 11, 12, 13, 14},
+				},
+			}},
+			[][]int{
+				[]int{87, 104, 121, 138, 155, 172, 189},
+				[]int{191, 228, 265, 302, 339, 376, 413},
+				[]int{295, 352, 409, 466, 523, 580, 637},
+				[]int{399, 476, 553, 630, 707, 784, 861},
+				[]int{503, 600, 697, 794, 891, 988, 1085},
+			},
+			false,
+		},
+		{
+			"invalid matrices",
+			args{[][][]int{
+				[][]int{
+					[]int{1, 2, 3, 4, 5},
+					[]int{6, 7, 8, 9, 10},
+					[]int{11, 12, 13, 14, 15},
+					[]int{16, 17, 18, 19, 20},
+				},
+				[][]int{
+					[]int{1, 2},
+					[]int{3, 4},
+				},
+			}},
+			nil,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := MatrixChainMultiply(tt.args.matrices)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("MatrixChainMultiply() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("MatrixChainMultiply() = %v, want %v", got, tt.want)
 			}
 		})
 	}
